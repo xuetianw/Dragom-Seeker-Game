2003a0f1c4553b017a83bc2c4fb61468e7e744c6
kazizi <kazizi@sfu.ca>
2018-02-02 20:00:07 -0800

    Initial Commit on Feb 2nd, 7:30 PM by Kourosh Azizi

---
+++ app/src/main/java/ca/cmpt276/as3/model/MainActivity.java
@@ -0,0 +1,34 @@
+package ca.cmpt276.as3.model;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setupLaunchButton();
+    }
+
+    private void setupLaunchButton(){
+        Button skipBtn = (Button) findViewById(R.id.skipBtnID);
+        skipBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Clicked on 'Go To Menu' ", Toast.LENGTH_SHORT)
+                        .show();
+                // Launch the Menu Activity:
+                Intent intent = MenuActivity.makeIntent(MainActivity.this);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/ca/cmpt276/as3/model/MenuActivity.java
@@ -0,0 +1,24 @@
+package ca.cmpt276.as3.model;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public static Intent makeIntent(Context context){
+        return new Intent(context, MenuActivity.class);
+    }
+
+}
