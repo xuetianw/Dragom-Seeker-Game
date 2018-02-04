0cbfccedf2722200f85838fa129b679f13581616
kazizi <kazizi@sfu.ca>
2018-02-03 23:32:54 -0800

    Initial Commit on Feb 2nd, 7:30 PM by Kourosh Azizi

--- app/src/main/java/ca/cmpt276/as3/model/MenuActivity.java
+++ app/src/main/java/ca/cmpt276/as3/model/MenuActivity.java
@@ -15,10 +15,80 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        setupGameScreen();        // for game screen, options and help
+        setupOptionsScreen();
+        setupHelpScreen();
     }
 
     public static Intent makeIntent(Context context){
         return new Intent(context, MenuActivity.class);
+    }
+
+    public void setupGameScreen(){
+        Button gameScreenBtn = (Button) findViewById(R.id.gameScreenID);
+        gameScreenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MenuActivity.this,"Clicked on 'Game Screen' ", Toast.LENGTH_SHORT)
+                        .show();
+                // Launch the Menu Activity:
+                Intent intent = GameScreen.makeIntent(MenuActivity.this);
+                startActivity(intent);
+            }
+        });
+
+//        Button optionsBtn = (Button) findViewById(R.id.optionsBtnID);
+//        optionsBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MenuActivity.this,"Clicked on 'Options' ", Toast.LENGTH_SHORT)
+//                        .show();
+//                // Launch the Menu Activity:
+//                Intent intent = GameScreen.makeIntent(MenuActivity.this);
+//                startActivity(intent);
+//            }
+//        });
+
+//        Button helpBtn = (Button) findViewById(R.id.helpBtnID);
+//        helpBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MenuActivity.this,"Clicked on 'Help' ", Toast.LENGTH_SHORT)
+//                        .show();
+//                // Launch the Menu Activity:
+//                Intent intent = GameScreen.makeIntent(MenuActivity.this);
+//                startActivity(intent);
+//            }
+//        });
+    }
+
+    public void setupOptionsScreen(){
+        Button optionsBtn = (Button) findViewById(R.id.optionsBtnID);
+        optionsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MenuActivity.this,"Clicked on 'Options' ", Toast.LENGTH_SHORT)
+                        .show();
+                // Launch the Menu Activity:
+                Intent intent = OptionsScreen.makeIntent(MenuActivity.this);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void setupHelpScreen(){
+        Button helpBtn = (Button) findViewById(R.id.helpBtnID);
+        helpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MenuActivity.this,"Clicked on 'Help' ", Toast.LENGTH_SHORT)
+                        .show();
+                // Launch the Menu Activity:
+                Intent intent = HelpScreen.makeIntent(MenuActivity.this);
+                startActivity(intent);
+            }
+        });
     }
 
 }
