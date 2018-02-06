b53ef6c967ade1a0339e037a748fce134bc3df15
kazizi <kazizi@sfu.ca>
2018-02-05 22:12:08 -0800

    Commit at Feb 5th, 10:15 PM by Kourosh Azizi

--- app/src/main/java/ca/cmpt276/as3/model/HelpActivity.java
+++ app/src/main/java/ca/cmpt276/as3/model/HelpActivity.java
@@ -1,9 +1,11 @@
 package ca.cmpt276.as3.model;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * This class is responsible to tell the user about the
@@ -17,10 +19,32 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+        setTextView();
     }
 
     public static Intent makeIntent(Context context){
         return new Intent(context, HelpActivity.class);
     }
+
+
+    @SuppressLint("SetTextI18n")        // ask if necessary
+    private void setTextView(){
+        TextView myTextView = (TextView) findViewById(R.id.aboutAuthorID);
+        myTextView.setText("This game was developed for a school project at " +
+                            "Simon Fraser University in Spring 2018. The developers " +
+                            "of this application are Kourosh Azizi and Fred Wu.");
+
+        myTextView = (TextView) findViewById(R.id.instructionsID);
+        myTextView.setText("Instructions: Instructions: Go to Main Menu and then " +
+                            "click on the Options button to choose your desired " +
+                            "board size and the number of mines that you want to " +
+                            "find in the game. Once you are done, go back to the " +
+                            "main menu and select on the Start Game button to begin " +
+                            "the game. Throughout the game, you can tap on each cell" +
+                            " to perform a scan in order to find out the number of mines." +
+                            " Each time you scan, there is a scan counter that will increase," +
+                            " you should try to win the round with less scans as possible.");
+    }
+
 
 }
--- app/src/main/java/ca/cmpt276/as3/model/OptionsActivity.java
+++ app/src/main/java/ca/cmpt276/as3/model/OptionsActivity.java
@@ -1,9 +1,15 @@
 package ca.cmpt276.as3.model;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class OptionsActivity extends AppCompatActivity {
 
@@ -11,9 +17,84 @@ public class OptionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+
+        setBoardSize();
+        setNumMines();
+        setupPrintButton();
     }
 
     public static Intent makeIntent(Context context){
         return new Intent(context, OptionsActivity.class);
+    }
+
+    private void setupPrintButton() {
+        Button btn1 = (Button) findViewById(R.id.printSelectedID);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // setting up the board size:
+                RadioGroup group = (RadioGroup) findViewById(R.id.radio_group_install_boardSize);
+                int idOfSelected = group.getCheckedRadioButtonId();
+                RadioButton radioButton = findViewById(idOfSelected);
+                String messageBoard = radioButton.getText().toString();
+
+                // setting up the number of mines:
+                group = (RadioGroup) findViewById(R.id.radio_group_install_mines);
+                idOfSelected = group.getCheckedRadioButtonId();
+                radioButton = findViewById(idOfSelected);
+                String messageMine = radioButton.getText().toString();
+
+                Toast.makeText(OptionsActivity.this, "Selected button's text is: "
+                        + messageBoard + " and " + messageMine, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @SuppressLint("SetTextI18n")        // necessary?
+    private void setBoardSize(){
+        RadioGroup group =(RadioGroup) findViewById(R.id.radio_group_install_boardSize);
+        int [] BoardRowArray = getResources().getIntArray(R.array.board_size);
+
+        // create the buttons:
+        for(int i = 0; i < BoardRowArray.length; i++){
+            final int boardRow = BoardRowArray[i];
+            RadioButton button = new RadioButton(this);
+            button.setText(boardRow + " " + getString(R.string.board_size));
+
+            // TODO: Set on-click callbacks
+            button.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(OptionsActivity.this, "You clicked " + boardRow, Toast.LENGTH_SHORT)
+                            .show();
+                }
+            });
+
+            // Add to radio group
+            group.addView(button);
+        }
+    }
+
+    @SuppressLint("SetTextI18n")        // necessary?
+    private void setNumMines(){
+        RadioGroup group =(RadioGroup) findViewById(R.id.radio_group_install_mines);
+        int [] numMinesArray = getResources().getIntArray(R.array.number_of_mines);
+        for(int i = 0; i < numMinesArray.length; i++){
+            final int numMines = numMinesArray[i];
+            RadioButton button = new RadioButton(this);
+            button.setText(numMines + " " + getString(R.string.number_of_mines));
+
+            // TODO: Set on-click callbacks
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(OptionsActivity.this, "You clicked " + numMines, Toast.LENGTH_SHORT)
+                            .show();
+                }
+            });
+
+            // Add to radio group
+            group.addView(button);
+        }
     }
 }
