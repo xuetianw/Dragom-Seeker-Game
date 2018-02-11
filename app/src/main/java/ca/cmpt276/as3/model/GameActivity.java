0ebf004b98227d8684cbb953459d6191f70cfe07
Fred <xuetianw@sfu.ca>
2018-02-10 21:49:05 -0800

    added basis of game buttons

--- app/src/main/java/ca/cmpt276/as3/model/GameActivity.java
+++ app/src/main/java/ca/cmpt276/as3/model/GameActivity.java
@@ -5,12 +5,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 /**
  * This class is responsible for allowing the user
  * to play the game.
  */
 public class GameActivity extends AppCompatActivity {
+    private static final int NUM_ROWS = 2;
+    private static final int NUM_COLS = 3;
     private String TAG = "OrientationDemo";
 
     @Override
@@ -18,6 +23,33 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         Log.e(TAG, "Running onCreate()!");  // test
+
+        populateButtons();
+    }
+
+    private void populateButtons() {
+        TableLayout table = (TableLayout) findViewById(R.id.tableForButtons);
+
+        for (int row =0;row <NUM_ROWS; row++){
+            TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    1.0f
+
+            ));
+            table.addView(tableRow);
+
+            for (int col = 0; col <NUM_COLS; col++){
+                Button button = new Button(this);
+                button.setLayoutParams(new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        1.0f));
+
+                tableRow.addView(button);
+            }
+        }
     }
 
     public static Intent makeIntent(Context context){
