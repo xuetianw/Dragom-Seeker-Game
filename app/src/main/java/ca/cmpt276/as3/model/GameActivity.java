ff478859963e4b574b491af872192fb6ff20526e
Fred <xuetianw@sfu.ca>
2018-02-10 22:59:53 -0800

    seted up gameActiciy background -added table layout, added gold image and click function -added scaling functions

--- app/src/main/java/ca/cmpt276/as3/model/GameActivity.java
+++ app/src/main/java/ca/cmpt276/as3/model/GameActivity.java
@@ -2,21 +2,29 @@ package ca.cmpt276.as3.model;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.Toast;
 
 /**
  * This class is responsible for allowing the user
  * to play the game.
  */
 public class GameActivity extends AppCompatActivity {
-    private static final int NUM_ROWS = 2;
-    private static final int NUM_COLS = 3;
+    private static final int NUM_ROWS = 15;
+    private static final int NUM_COLS = 5;
     private String TAG = "OrientationDemo";
+
+    Button buttons[][] = new Button[NUM_ROWS][NUM_COLS];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +55,58 @@ public class GameActivity extends AppCompatActivity {
                         TableRow.LayoutParams.MATCH_PARENT,
                         1.0f));
 
+                button.setText(col + "," + row);
+                //make text not clip on small buttons
+                button.setPadding(0,0,0,0);
+
+                final int finalCol = col;
+                final int finalRow = row;
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        gridButtonClicked(finalCol, finalRow);
+                    }
+                });
+
                 tableRow.addView(button);
+                buttons[row][col] = button;
+            }
+        }
+    }
+
+    private void gridButtonClicked(int col, int row) {
+        Toast.makeText(this, "Button clicked: " + col + "," + row,
+                Toast.LENGTH_SHORT).show();
+        Button button = buttons [row][col];
+
+        // Lock Button Sizes:
+        lockButtonSizes();
+
+
+        //Scale Image to button
+        int newWidth = button.getWidth();
+        int newHeight = button.getHeight();
+        Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.gold);
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(originalBitmap, newWidth, newHeight, true);
+        Resources resource = getResources();
+        button.setBackground(new BitmapDrawable(resource, scaledBitmap));
+
+        // change text
+        button.setText("" + col);
+    }
+
+    private void lockButtonSizes() {
+        for (int row =0;row <NUM_ROWS; row++){
+            for (int col = 0; col <NUM_COLS; col++){
+                Button button = buttons[row][col];
+
+                int width = button.getWidth();
+                button.setMinWidth(width);
+                button.setMaxWidth(width);
+
+                int height = button.getHeight();
+                button.setMinHeight(height);
+                button.setMaxHeight(height);
             }
         }
     }
