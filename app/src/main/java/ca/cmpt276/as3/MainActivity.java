8737c2b1817428d4496cdd1af65b571cca62f6d1
kazizi <kazizi@sfu.ca>
2018-02-13 14:33:37 -0800

    Commit at Feb 11th, 1:30 PM by Kourosh Azizi fixed the animation and the radio button colors

--- app/src/main/java/ca/cmpt276/as3/MainActivity.java
+++ app/src/main/java/ca/cmpt276/as3/MainActivity.java
@@ -1,29 +1,26 @@
 package ca.cmpt276.as3;
 
-import android.animation.Animator;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewPropertyAnimator;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import ca.cmpt276.as3.model.R;
 
 public class MainActivity extends AppCompatActivity {
-    private String TAG = "OrientationDemo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String TAG = "OrientationDemo";
         Log.e(TAG, "Running onCreate()!");  // test
         setUpImageAnimation();
         setupLaunchButton();
@@ -55,71 +52,23 @@ public class MainActivity extends AppCompatActivity {
         myImageView.setImageResource(R.drawable.background_image3);
     }
 
-    /////////////////// adding code ////////////////////
-
-    // development:
-    private void setUpAnimation(){
-        LinearLayout ll = new LinearLayout(this);
-
-        ll.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT));
-
-        ll.setOrientation(LinearLayout.VERTICAL);
-
-        final TextView tv = new TextView(this);
-        tv.setText("Animation");
-
-        final TranslateAnimation moveLefttoRight =
-                new TranslateAnimation(0, 200, 0, 0);
-
-        moveLefttoRight.setDuration(1000);
-        moveLefttoRight.setFillAfter(true);
-        tv.startAnimation(moveLefttoRight);
-
-        ll.addView(tv);
-        setContentView(ll);
-    }
-
-    // development:
     private void setUpImageAnimation(){
         ImageView imageAnimation = (ImageView) findViewById(R.id.animationID);
         TranslateAnimation animation = new TranslateAnimation(400.0f, 1100.0f,
                 0.0f, 400.0f);
-        animation.setDuration(2000);
+        animation.setDuration(1000);
         animation.setRepeatCount(4);
-        animation.setRepeatMode(2);   // repeat animation L to R and R to L
+        animation.setRepeatMode(2);                 // repeat animation L to R and R to L
+        imageAnimation.startAnimation(animation);
+        imageAnimation.setVisibility(View.INVISIBLE);
 
-
-        ImageView logo = (ImageView)findViewById(R.id.animationID);
-        ViewPropertyAnimator anim = logo.animate();
-        anim.setListener(new Animator.AnimatorListener() {
+        Handler useHandler  = new Handler();
+        useHandler.postDelayed(new Runnable() {
             @Override
-            public void onAnimationStart(Animator animator) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animator) {
-                Toast.makeText(MainActivity.this,"yooooooooo "
-                        , Toast.LENGTH_SHORT).show();
+            public void run() {
                 Intent intent = MenuActivity.makeIntent(MainActivity.this);
                 startActivity(intent);
             }
-
-            @Override
-            public void onAnimationCancel(Animator animator) {
-//                Intent intent = MenuActivity.makeIntent(MainActivity.this);
-//                startActivity(intent);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animator) {
-
-            }
-
-        });
-        imageAnimation.startAnimation(animation);
-        imageAnimation.setVisibility(View.INVISIBLE);
+        },4500);
     }
-
 }
